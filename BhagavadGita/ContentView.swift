5c0d3640c9cd9709ae8e32c1a157b1d13a6aea27
n-sivaram <sinakka@fiskerinc.com>
2023-02-12 16:21:49 +0530

    Initial Commit

---
+++ BhagavadGita/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  BhagavadGita
+//
+//  Created by Sivaramaiah NAKKA on 12/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
