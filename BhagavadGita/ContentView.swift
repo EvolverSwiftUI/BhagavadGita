b7c119976b4dd0c11e37c60d3bfac33a8ddf252e
n-sivaram <sinakka@fiskerinc.com>
2023-02-12 16:48:05 +0530

    Added package to our project

--- BhagavadGita/ContentView.swift
+++ BhagavadGita/ContentView.swift
@@ -6,10 +6,15 @@
 //
 
 import SwiftUI
+import BGUtility
 
 struct ContentView: View {
     var body: some View {
         Text("Hello, World!")
+            .onAppear {
+                let nums = [1,3,7,8]
+                print(nums[safeindex: 0] ?? -1)
+            }
     }
 }
 
